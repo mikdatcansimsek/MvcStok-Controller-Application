7c79b3449056d5d44772180e16499e0f87983938
Mikdat Can Şimşek <1030521058@erciyes.edu.tr>
2023-10-24 15:38:07 +0300

    Add project files.

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities();
+
+        public ActionResult Index()
+        {
+            var degerler=db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+
+        public ActionResult Index()
+        {
+            var degiskenler = db.TBLMUSTERILER.ToList();
+            return View(degiskenler);
+        }
+    }
+}
