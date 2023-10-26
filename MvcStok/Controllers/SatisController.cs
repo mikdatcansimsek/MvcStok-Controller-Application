1b8fb8aef81a683c903b4fa51a191cac8eee5df7
Mikdat Can Şimşek <1030521058@erciyes.edu.tr>
2023-10-26 16:47:17 +0300

    26.10.2023 16:47

--- MvcStok/Controllers/SatisController.cs
+++ MvcStok/Controllers/SatisController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcStok.Models.Entity;
 
 namespace MvcStok.Controllers
 {
     public class SatisController : Controller
     {
+        MvcDbStokEntities db = new MvcDbStokEntities();
         // GET: Satis
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p1)
+        {
+            db.TBLSATISLAR.Add(p1);
+            db.SaveChanges();
+            return View("Index");
         }
     }
 }
