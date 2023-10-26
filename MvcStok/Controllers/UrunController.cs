fb4210e9ee904095431805005453b353fe945fb4
Mikdat Can Şimşek <1030521058@erciyes.edu.tr>
2023-10-26 16:04:44 +0300

    26.10.2023

--- MvcStok/Controllers/UrunController.cs
+++ MvcStok/Controllers/UrunController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Management;
 using System.Web.Mvc;
 using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
 
 namespace MvcStok.Controllers
 {
@@ -11,10 +14,67 @@ namespace MvcStok.Controllers
     {
         // GET: Urun
         MvcDbStokEntities db = new MvcDbStokEntities();
-        public ActionResult Index()
+        public ActionResult Index(int sayfa=1)
         {
-            var degerler = db.TBLURUNLER.ToList();
+            //var degerler = db.TBLURUNLER.ToList();
+            var degerler = db.TBLURUNLER.ToList().ToPagedList(sayfa, 4);
             return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler=(from i in db.TBLKATEGORILER.ToList()
+                select new SelectListItem
+                {
+                    Text=i.KATEGORIAD,
+                    Value=i.KATEGORIID.ToString()
+                }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Where(i => i.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            p1.TBLKATEGORILER = ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SIL(int id)
+        {
+            var urun= db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View("UrunGetir", urun);
+        }
+        public ActionResult Guncelle(TBLURUNLER p1)
+        {
+            var urun = db.TBLURUNLER.Find(p1.URUNID);
+            urun.URUNAD = p1.URUNAD;
+            urun.MARKA = p1.MARKA;
+            urun.STOK = p1.STOK;
+            urun.FIYAT = p1.FIYAT;
+            //urun.URUNKATEGORI = p1.URUNKATEGORI;
+            var ktg = db.TBLKATEGORILER.Where(i => i.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();
+            urun.URUNKATEGORI = ktg.KATEGORIID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
--- MvcStok/Models/Entity/TBLMUSTERILER.cs
+++ MvcStok/Models/Entity/TBLMUSTERILER.cs
@@ -11,7 +11,8 @@ namespace MvcStok.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TBLMUSTERILER
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,9 @@ namespace MvcStok.Models.Entity
         }
     
         public int MUSTERIID { get; set; }
+
+        [Required(ErrorMessage ="Bu Alaný Boþ Býrakazmasýnýz...")]
+        [StringLength(50,ErrorMessage ="En Fazla 50 Karaketerlik Ýsim Girin")]
         public string MUSTERIAD { get; set; }
         public string MUSTERISOYAD { get; set; }
     
